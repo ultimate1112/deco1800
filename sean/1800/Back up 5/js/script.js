44b1fb61a70c07b8e27702ee6c603b460ffc599c
Anthony Chiam <ultimate1112@mail.com>
2020-10-23 08:40:00 +0000

    Backup

---
+++ sean/1800/Back up 5/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //"JavaScript is Enabled" Body Class
+    $("body").addClass("js");
+
+    // 'Compare' Button 
+    $('#compare-btn').on('click', function(event){
+        $(this).toggleClass('btn-checked');
+        $('#fltr-location-2').toggleClass('d-none');
+    });
+
+    // Form Submit Button
+    $('#submit-btn').on('click', function(event) {
+        event.preventDefault();
+        $(this).addClass('btn-checked');
+    });
+});
